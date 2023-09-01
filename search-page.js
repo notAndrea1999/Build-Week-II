8e993af6f199554c4f33c58fff6432a27f836af3
AndreaFerrara13 <138196137+AndreaFerraray@users.noreply.github.com>
2023-09-01 11:14:04 +0200

    Revert "search"
    This reverts commit 3022159a0df6ca97635b731b24a185d1a57d1a4e.

--- search-page.js
+++ search-page.js
@@ -1,4 +1,4 @@
-const URL = " https://striveschool-api.herokuapp.com/api/deezer/search?q=rock";
+const URL = " https://striveschool-api.herokuapp.com/api/deezer/search?q=queen";
 
 const options = {
   method: "GET",
@@ -27,7 +27,7 @@ const caricamento = async () => {
 
     // console.log(data);
     // console.log(data2);
-    album.classList.remove("d-none");
+
     data.forEach((data) => {
       console.log(data.album.title);
       const col = document.createElement("div");
@@ -62,9 +62,9 @@ const caricamento = async () => {
 
     data2.forEach((data2) => {
       const col2 = document.createElement("div");
-      col2.className = "col  gy-3";
+      col2.className = "col gy-3";
       col2.innerHTML = `
-    <div class=" card-body-2 card border border-0 ">
+    <div class="card-body-2 card border border-0 ">
     <a href="./album.html?albumId=${data2.album.id}" class="text-light"><img src="${data2.album.cover_medium}" class="card-img-top" alt="..." /></a>
       <div class=" card-body-text text-light">
         <h5 class="card-title"><a href="./album.html?albumId=${data2.album.id}" class="text-light">${data2.album.title}</a></h5>
@@ -126,82 +126,58 @@ const goPlayer = async (event) => {
   console.log(fullData);
 };
 
-// -------------------default-----------------------------
-// -----------------------------
-// ---------------------------
-window.onload = async () => {
-  try {
-    event.preventDefault();
-    const row = document.getElementById("row");
-    const row2 = document.getElementById("row-2");
+// window.onload = async () => {
+//   try {
+//     // event.preventDefault();
+//     const row = document.getElementById("row");
+//     const row2 = document.getElementById("row-2");
+//     row.innerHTML = "";
+//     row2.innerHTML = "";
 
-    const resp = await fetch(URL, options);
-    // console.log(resp);
+//     const resp = await fetch(URL, options);
+//     // console.log(resp);
 
-    const infos = await resp.json();
-    console.log(infos);
+//     const infos = await resp.json();
+//     console.log(infos);
 
-    const data = infos.data.slice(0, 6);
-    const data2 = infos.data.slice(0, 25);
+//     const data = infos.data.slice(0, 6);
+//     const data2 = infos.data.slice(0, 5);
 
-    // console.log(data);
-    // console.log(data2);
-    album.classList.add("d-none");
-    data.forEach((data) => {
-      console.log(data.album.title);
-      const col = document.createElement("div");
-      col.className = "col col-lg-4 col-md-6 col-6 gy-2 d-none";
-      col.innerHTML = `
-       <div class="card border border-0 d-flex flex-row">
-       <a href="./album.html?albumId=${data.album.id}"><img class="image-type-1 img-card-top" src="${data.album.cover_medium}" alt="" /></a>
-       <div class="card-body"><a href="./album.html?albumId=${data.album.id}" class="text-light">${data.album.title}</a></div>
-       </div>
-       `;
-      row.appendChild(col);
-    });
+//     // console.log(data);
+//     // console.log(data2);
 
-    data2.forEach((data2) => {
-      const col2 = document.createElement("div");
-      col2.className = "col gy-3";
-      col2.innerHTML = `
-      
-     
-    <div class=" card-body-2 card border border-0 ">
-    
-    <a href="./album.html?albumId=${data2.album.id}" class="text-light"><img id='imgDefaultSearch' src="${data2.album.cover_medium}" class="card-img-top img-fluid " alt="..." /></a>
-      
-    </div>
-    
-    
-    
-      
-    
-      `;
-      row2.appendChild(col2);
-    });
-    //   data.forEach((data) => {
-    //     const col2 = document.createElement("div");
-    //     col2.className = "col gy-3";
-    //     col2.innerHTML = `
-    //     <div  class='bg-danger d-flex'>
-    //     <h5 class="card-title"><a href="./album.html?albumId=${dataa.album.id}" class="z-2 position-absolute text-light">${dataa.album.title}</a></h5>
-    //   <div class=" card-body-2 card border border-0 ">
+//     data.forEach((data) => {
+//       console.log(data.album.title);
+//       const col = document.createElement("div");
+//       col.className = "col col-lg-4 col-md-6 gy-2";
+//       col.innerHTML = `
+//        <div class="card border border-0 d-flex flex-row">
+//        <a href="./album.html?albumId=${data.album.id}"><img class="image-type-1 img-card-top" src="${data.album.cover_medium}" alt="" /></a>
+//        <div class="card-body"><a href="./album.html?albumId=${data.album.id}" class="text-light">${data.album.title}</a></div>
+//        </div>
+//        `;
+//       row.appendChild(col);
 
-    //   <a href="./album.html?albumId=${dataa.album.id}" class="text-light"><img id='imgDefaultSearch' src="${dataa.album.cover_medium}" class="card-img-top img-fluid " alt="..." /></a>
+//       //   const firstAlbum = document.getElementById("firstAlbum-body");
+//       album1.innerHTML = `
+//       <div class="img pe-3">
+//       <a href="./album.html?albumId=${data.album.id}"><img src="${data.album.cover_medium}" alt="" /></a>
+//       </div>
+//       <div class="album-body">
+//       <h3>${data.album.title}</h3>
+//       <h1>${data.title}</h1>
+//       <p><a class="text-light" href="./artistpage.html?artistId=${data.artist.id}">${data.artist.name}</a></p>
+//       <p>Ascolta il nuovo brano di ${data.artist.name}</p>
+//       <div class="buttons d-flex align-items-center">
+//         <button onclick="goPlayer(event)" class="btn btn-success rounded-pill play" type="button">Play</button>
+//         <button class="btn btn-primary rounded-pill bg-black salva" type="buton">Salva</button>
+//         <button class="btn rounded-pill bg-black dots" type="buton">...</button>
+//       </div>
+//     </div>
 
-    //   </div>
-    //   </div>
-    //   <div class='bg-warning d-flex justify-content-center" '>
-    //   <h5 class="card-title"><a href="./album.html?albumId=${dataa.album.id}" class="z-2 position-absolute text-light">${dataa.album.title}</a></h5>
-    //       <div  class="  card-body-2 card border border-0 ">
-    //   <a href="./album.html?albumId=${dataa.album.id}" class="text-light"><img src="${dataa.album.cover_medium}" class="card-img-top img-fluid" alt="..." /></a>
-
-    //   </div>
-    //   </div>
-    //     `;
-    //     row2.appendChild(col2);
-    //   });
-  } catch (error) {
-    console.log(error);
-  }
-};
+//       `;
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
